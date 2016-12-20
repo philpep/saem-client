727a74c9aa4d5f3942d55b55eff3f582890c4866
Sylvain Thenault <sylvain.thenault@logilab.fr>
2016-12-20 16:44:32 +0100

    Split add_generic_arguments into a really generic function and a OAI specific one

--- saem_client/__init__.py
+++ saem_client/__init__.py
@@ -17,12 +17,16 @@ def _oai_client(url, prefix):
 
 def _add_generic_arguments(parser):
     parser.add_argument('url', help='base URL of the SAEM-Ref instance')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='display progress information')
+
+
+def _add_oai_arguments(parser):
+    _add_generic_arguments(parser)
     parser.add_argument('-o', '--output', default=os.getcwd(),
                         help='output directory (default to current directory)')
     parser.add_argument('--limit', type=int,
                         help='fetch at much LIMIT records')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='display progress information')
 
 
 def main():
@@ -32,16 +36,19 @@ def main():
 
     parser = ArgumentParser(description=__doc__)
     subparsers = parser.add_subparsers()
+
     eac_parser = subparsers.add_parser(
         'eac-download', help='download EAC-CPF authority records')
     eac_parser.set_defaults(func=fetch_eac_records)
-    _add_generic_arguments(eac_parser)
+    _add_oai_arguments(eac_parser)
+
     skos_parser = subparsers.add_parser(
         'skos-download', help='download a SKOS concept scheme')
-    _add_generic_arguments(skos_parser)
+    skos_parser.set_defaults(func=fetch_concepts)
+    _add_oai_arguments(skos_parser)
     skos_parser.add_argument(
         'scheme', help='identifier of the concept scheme')
-    skos_parser.set_defaults(func=fetch_concepts)
+
     eac_upload = subparsers.add_parser(
         'eac-upload', help='upload an EAC-CPF file')
     eac_upload.set_defaults(func=upload_eac)
@@ -50,6 +57,7 @@ def main():
         help='identifier of the SAEM-Ref instance defined in cwclientlibrc file')
     eac_upload.add_argument(
         'file', help='file path of the EAC-CPF file to upload')
+
     args = parser.parse_args()
     func = args.func
     del args.func
